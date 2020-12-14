e9f3e74c77d5188301f406ddd9105d282f30d271
JamesPagani <jaimeagalvezv@gmail.com>
2020-12-14 16:50:21 -0500

    Playing with C# exception handling capabilities

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int printedItems = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            try
+            {
+                Console.WriteLine(myList[i]);
+                printedItems++;
+            }
+            catch (System.Exception)
+            {
+                
+                return printedItems;
+            }
+        }
+
+        return printedItems;
+    }
+}
---
+++ 0x04-csharp-exceptions/README.md
@@ -0,0 +1,6 @@
+# 0x04. C# - Exceptions
+
+## About
+This directory tackles C# method of handling exceptions.
+
+**Jaime Andrés Gálvez Villamarin**
