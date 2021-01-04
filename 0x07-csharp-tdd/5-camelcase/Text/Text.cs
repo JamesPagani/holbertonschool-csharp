b7d55a2cb3d3428b560ad49636445a15a87e0753
JamesPagani <jaimeagalvezv@gmail.com>
2021-01-04 17:45:51 -0500

    Corrected a comment inside the CamelCase function

--- 0x07-csharp-tdd/5-camelcase/Text/Text.cs
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -9,7 +9,7 @@ namespace Text
         ///<remark>In camel case, every word begins with a capital letter except the first one.</remark>
         public static int CamelCase(string s)
         {
-            ///Determines if there if the string isn't empty.
+            //Determines if there if the string isn't empty.
             int wordsFound = s.Length > 0 ? 1 : 0;
 
             foreach (char letter in s)
