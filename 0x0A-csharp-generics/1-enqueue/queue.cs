2aa3c17e189a86fccec771a84b70d0ad83edfa16
JamesPagani <jaimeagalvezv@gmail.com>
2021-03-22 19:16:58 -0500

    0x0A, Generics project. Created generic queue structure, with enqueue operation

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Queue<T>
+{
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public Node Head
+    {
+        get {return head;}
+        set {head = value;}
+    }
+    public Node Tail
+    {
+        get {return tail;}
+        set {tail = value;}
+    }
+
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public void Enqueue(T newValue)
+    {
+        Node newNode = new Node(newValue);
+        if (head == null)
+        {
+            head = newNode;
+            newNode.Next = null;
+            tail = newNode;
+        }
+        else
+        {
+            newNode.Next = head;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+
+    public class Node
+    {
+        private T value = default(T);
+        private Node next = null;
+
+        public T Value
+        {
+            get {return this.value;}
+            set {this.value = value;}
+        }
+        public Node Next
+        {
+            get {return next;}
+            set {next = value;}
+        }
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+}
