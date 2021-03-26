1faa7fcef000ac44a535ce4d50b51ea84c2f845e
JamesPagani <jaimeagalvezv@gmail.com>
2021-03-25 20:42:53 -0500

    Minor modifications

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -4,25 +4,22 @@ using System;
 public class Player
 {
     // Player name
-    private string name {get; set;}
+    string name {get; set;}
 
     // Maximum hit points
-    private float maxHP {get; set;}
+    float maxHP {get; set;}
 
     // Current hit points
-    private float hp {get; set;}
+    float hp {get; set;}
 
     ///<summary>Initiate a new player instance.</summary>
     public Player(string name = "Player", float maxHP = 100f){
         if (maxHP <= 0)
         {
-            this.maxHP = 100f;
+            maxHP = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
-        else
-        {
-            this.maxHP = maxHP;
-        }
+        this.maxHP = maxHP;
         this.hp = this.maxHP;
         this.name = name;
     }
