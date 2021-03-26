e17bce6961098107be53c9370b92adee19f9e853
JamesPagani <jaimeagalvezv@gmail.com>
2021-03-25 20:59:47 -0500

    Turned the properties into fields

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -7,26 +7,27 @@ public class Player
     string name;
 
     // Maximum hit points
-    float maxHP;
+    float maxHp;
 
     // Current hit points
     float hp;
 
     ///<summary>Initiate a new player instance.</summary>
-    public Player(string name = "Player", float maxHP = 100f){
-        if (maxHP <= 0)
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
         {
-            maxHP = 100f;
+            maxHp = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
-        this.maxHP = maxHP;
-        this.hp = this.maxHP;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
         this.name = name;
     }
 
     ///<summary>Print to the console the health of this player.</summary>
     public void PrintHealth()
     {
-        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHP} health");
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
     }
 }
